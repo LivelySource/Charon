994563f6e706214c58a219d1df5791c8625248f7
LivelySource <weston.schmidt4@gmail.com>
2018-10-22 12:48:04 -0700

    go

--- modules/functions.js
+++ modules/functions.js
@@ -78,7 +78,7 @@ module.exports = (client) => {
     text = text
       .replace(/`/g, "`" + String.fromCharCode(8203))
       .replace(/@/g, "@" + String.fromCharCode(8203))
-      .replace(client.token, "mfa.VkO_2G4Qv3T--NO--lWetW_tjND--TOKEN--QFTm6YGtzq9PH--4U--tG0");
+      .replace(client.token, process.env.token);
 
     return text;
   };
