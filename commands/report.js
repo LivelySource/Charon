74806def9660f9e03fe00bb3985af63c63487bde
LivelySource <weston.schmidt4@gmail.com>
2018-11-01 20:17:57 -0700

    yeet

--- commands/report.js
+++ commands/report.js
@@ -3,7 +3,7 @@
 const Discord = require("discord.js");
 const Report = require("../model/report.js");
 const mongoose = require("mongoose");
-mongoose.createConnection(process.env.reports);
+mongoose.createConnection(process.env.reports, { useNewUrlParser: true });
 module.exports.run = async (bot, message, args) => {
   //-report @_Lively (Reason)
 
