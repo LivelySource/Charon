97dd2bd8a7dd6b7fcd25d670dddb00ded24a8ab0
LivelySource <weston.schmidt4@gmail.com>
2018-11-03 20:29:40 -0700

    jesus h. christ

--- commands/ban.js
+++ commands/ban.js
@@ -6,7 +6,7 @@ mongoose.connect(process.env.reports, {
 });
 module.exports.run = async (bot, message, args) => {
 
-      // -ban @_Lively#0286
+      // -ban @_Lively#0286 <reason>
 
       let bUser = message.mentions.members.first()
       if(!bUser) return message.channel.send("Can't find user!");
