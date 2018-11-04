0436f0c4b47eac50e777545315b140cea2faa160
LivelySource <weston.schmidt4@gmail.com>
2018-11-03 23:03:44 -0700

    post

--- commands/finduser.js
+++ commands/finduser.js
@@ -1,10 +1,10 @@
 module.exports.run = async (bot, message, args) => {
-    let users = bot.users;
 
+    let users = bot.users;
     let searchTerm = args[0];
-    let founduser = message.mentions.members.tag.first();
     if(!searchTerm) return message.channel.send("Please type a term to search!");
-    if (!founduser) return message.channel.send("It seems that you already know their tag!")
+    let founduser = message.mentions.members.tag
+    if (!founduser) return message.channel.send("It seems that you already know their tag!");
     let matches = users.filter(u => u.tag.toLowerCase().includes(searchTerm.toLowerCase()));
     
     message.channel.send("Is this who who are looking for?" + matches.map(u => u.tag));
