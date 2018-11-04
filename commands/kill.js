db8bf8e47bc683a6d5a39bcbc6f8aab044923f37
LivelySource <weston.schmidt4@gmail.com>
2018-11-03 22:10:17 -0700

    trynow

--- commands/kill.js
+++ commands/kill.js
@@ -8,18 +8,18 @@ if(!killed) {
 let suicide = new Discord.RichEmbed()
 .setColor("#00f00")
 .setDescription(`${message.author} decied to kill themself 💔 REST IN PEACE`)
-.setThumbnail("https://discordapp.com/channels/472174636839469088/494751952480108546/508506268537847809")
+.setThumbnail("https://discordapp.com/channels/472174636839469088/494751952480108546/508506268537847809");
 
-message.channel.send(suicide)
+message.channel.send(suicide);
 
 } else {
 
 let murder = new Discord.RichEmbed()
 .setColor("#00f00")
 .setDescription(`${killed} was killed by ${message.author} 💔 REST IN PEACE`)
-.setThumbnail("https://discordapp.com/channels/472174636839469088/494751952480108546/508506268537847809")
+.setThumbnail("https://discordapp.com/channels/472174636839469088/494751952480108546/508506268537847809");
 
-message.channel.send(murder)
+message.channel.send(murder);
 
 }
 exports.conf = {
