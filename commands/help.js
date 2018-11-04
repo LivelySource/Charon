d581f6305cf80a38926f45e6930ce618d6c28a91
LivelySource <weston.schmidt4@gmail.com>
2018-11-03 20:01:21 -0700

    go asshole!

--- commands/help.js
+++ commands/help.js
@@ -1,12 +1,3 @@
-<<<<<<< HEAD
-=======
-/*
-The HELP command is used to display every command's name and description
-to the user, so that he may see what commands are available. The help
-command is also filtered by level, so if a user does not have access to
-a command, it is not shown to them. If a command name is given with the
-help command, its extended help is shown.
-*/
 
 exports.run = (client, message, args, level) => {
   // If no specific command is called, show all filtered commands.
@@ -14,16 +5,15 @@ exports.run = (client, message, args, level) => {
     // Load guild settings (for prefixes and eventually per-guild tweaks)
     const settings = message.settings;
 
-    // Filter all commands by which are available for the user's level, using the <Collection>.filter() method.
+
     const myCommands = message.guild ? client.commands.filter(cmd => client.levelCache[cmd.conf.permLevel] <= level) : client.commands.filter(cmd => client.levelCache[cmd.conf.permLevel] <= level &&  cmd.conf.guildOnly !== true);
 
-    // Here we have to get the command names only, and we use that array to get the longest name.
     // This make the help commands "aligned" in the output.
     const commandNames = myCommands.keyArray();
     const longest = commandNames.reduce((long, str) => Math.max(long, str.length), 0);
 
     let currentCategory = "";
-    let output = `!========== [Charon's_Commands] ==========!\nFor any further help, direct message _Lively#0286\n\n[Use ${settings.prefix}help <commandname> for details]\n`;
+    let output = `!========== [Charon's Commands] ==========!\nFor any further help, direct message _Lively#0286\n\n[Use ${settings.prefix}help <commandname> for details]\n`;
     const sorted = myCommands.array().sort((p, c) => p.help.category > c.help.category ? 1 :  p.help.name > c.help.name && p.help.category === c.help.category ? 1 : -1 );
     sorted.forEach( c => {
       const cat = c.help.category.toProperCase();
@@ -58,4 +48,4 @@ exports.help = {
   description: "Displays all the available commands for your permission level.",
   usage: "help [command]"
 };
->>>>>>> parent of 5407f20... post
+
