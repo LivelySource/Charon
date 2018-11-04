ab8e0cb361dfaa61a528b3d1c4aea1cb31c77ec5
LivelySource <weston.schmidt4@gmail.com>
2018-11-04 00:41:48 -0700

    push!

---
+++ commands/menu.js
@@ -0,0 +1,58 @@
+const Discord = require('discord.js'); 
+ //Optional
+
+    module.exports.run = async (bot, message, args, tools) => {
+  let pages = ['page 1', 'page 2', 'page 3', 'page 4', 'All you need to do is add another item in the array', '**Supports markdown and regular chat description properties**']; 
+  let page = 1; 
+ 
+  const embed = new Discord.RichEmbed() 
+    .setColor(0xffffff)
+    .setFooter(`Page ${page} of ${pages.length}`) 
+    .setDescription(pages[page-1])
+ 
+  message.channel.send(embed).then(msg => { 
+   
+    msg.react('⏪').then( r => { 
+      msg.react('⏩') 
+     
+      const backwardsFilter = (reaction, user) => reaction.emoji.name === '⏪' && user.id === message.author.id;
+      const forwardsFilter = (reaction, user) => reaction.emoji.name === '⏩' && user.id === message.author.id; 
+     
+      const backwards = msg.createReactionCollector(backwardsFilter, { time: 60000 }); 
+      const forwards = msg.createReactionCollector(forwardsFilter, { time: 60000 }); 
+     
+      
+      backwards.on('collect', r => { 
+        if (page === 1) return; 
+        page--; 
+        embed.setDescription(pages[page-1]); 
+        embed.setFooter(`Page ${page} of ${pages.length}`); 
+        msg.edit(embed) 
+      })
+     
+      forwards.on('collect', r => { 
+        if (page === pages.length) return; 
+        page++; 
+        embed.setDescription(pages[page-1]); 
+        embed.setFooter(`Page ${page} of ${pages.length}`); 
+        msg.edit(embed) 
+      })
+   
+    })
+ 
+  })
+ 
+}
+exports.conf = {
+        enabled: true,
+        guildOnly: true,
+        aliases: [],
+        permLevel: "Bot Admin"
+      };
+      
+      exports.help = {
+        name: "menu",
+        category: "System",
+        description: "Command in development",
+        usage: "menu"
+      };
