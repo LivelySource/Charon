67e48718779a0c648b3334eecec00f3d28256926
LivelySource <weston.schmidt4@gmail.com>
2018-11-03 19:51:37 -0700

    Revert "push"
    This reverts commit fce197899f44c8f5b0d0660033c806a7693d3d31.

--- commands/help.js
+++ commands/help.js
@@ -22,15 +22,23 @@ exports.run = (client, message, args, level) => {
 
     let currentCategory = "";
 <<<<<<< HEAD
+<<<<<<< HEAD
     let output = `!========== [Charon's Commands] ==========!\n\n[Use ${settings.prefix}help <commandname> for details]`;
 =======
     let output = `!==========< Charon's Commands >==========!\n\nFor any further help, direct message _Lively#0286\n\n[Use ${settings.prefix}help <commandname> for details]\n`;
 >>>>>>> parent of f56e1e4... Help improved
+=======
+    let output = `!==========[Charon's commands]==========!\n\nFor any further help, direct message _Lively#0286\n\n[Use ${settings.prefix}help <commandname> for details]\n`;
+>>>>>>> parent of fce1978... push
     const sorted = myCommands.array().sort((p, c) => p.help.category > c.help.category ? 1 :  p.help.name > c.help.name && p.help.category === c.help.category ? 1 : -1 );
     sorted.forEach( c => {
       const cat = c.help.category.toProperCase();
       if (currentCategory !== cat) {
+<<<<<<< HEAD
         output += `\n--- ${cat} ---\n`;
+=======
+        output += `\n--- ${cat} ==---\n`;
+>>>>>>> parent of fce1978... push
         currentCategory = cat;
       }
       output += `${settings.prefix}${c.help.name}${" ".repeat(longest - c.help.name.length)} :: ${c.help.description}\nFor any further help, direct message _Lively#0286\n================================================`;
