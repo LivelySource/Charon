b8f37c60d69e5fdf5c9877bddca4e09c8f9d5c5d
LivelySource <weston.schmidt4@gmail.com>
2018-11-04 16:50:30 -0800

    poooost

--- index.js
+++ index.js
@@ -39,7 +39,7 @@ const log = message => {
 
 //Odols Currency
 client.on("message", (message) => {
-  (message.content.contains())
+  (message.content.endWith("."))
   let odolstoadd = Math.ceil(Math.random() * 2);
   console.log(odolstoadd + " odols")
   Odols.findOne({userID: message.author.id, serverID: message.guild.id}, (err, odols) =>{
