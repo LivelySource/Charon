d466a4acfdd82f48ed6211ed0ad190504c829598
LivelySource <weston.schmidt4@gmail.com>
2018-11-15 17:55:08 -0800

    push!

--- commands/verify.js
+++ commands/verify.js
@@ -1,8 +1,8 @@
 const Discord = require("discord.js");
 
 module.exports.run = async (bot, message, args) => {
-let guild = client.guilds.find(guild => guild.name === "Charon Support 💀");
-if (guild == false){
+
+if (client.guilds.find(guild => guild.name === "Charon Support 💀") == false){
         return message.reply('This command is exculsively for the Charon, support server! Try ${settings.prefix}support');
     };
   //-verify
