e7b23852ded2fe536a9b0ee0069a3f23f00b8d23
LivelySource <weston.schmidt4@gmail.com>
2018-12-06 16:13:34 -0800

    Anti-Spam system

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ const { promisify } = require("util");
 const readdir = promisify(require("fs").readdir);
 const Enmap = require("enmap");
 const EnmapLevel = require("enmap-level");
+const antispam = require("discord-anti-spam");
 const mongoose = require("mongoose");
 //Establishing a connection with MongoDB...
 mongoose.connect(process.env.reports, {
@@ -36,6 +37,16 @@ const log = message => {
   console.log(`[${moment().format('YYYY-MM-DD HH:mm:ss')}] ${message}`);
 };
 
+antispam(bot, {
+  warnBuffer: 3,
+  maxBuffer: 5, 
+  interval: 1000, // Amount of time in ms users can send a maximum of the maxBuffer variable before getting banned.
+  warningMessage: " , Stop spamming! If you continue I'll be forced to ban you!", // Warning message send to the user indicating they are going to fast.
+  banMessage: " was spamming even after I had warned them. Let this be a lesson, don't spam!", // Ban message, always tags the banned user in front of it.
+  maxDuplicatesWarning: 7, // Maximum amount of duplicate messages a user can send in a timespan before getting warned
+  maxDuplicatesBan: 10, // Maximum amount of duplicate messages a user can send in a timespan before getting banned
+  deleteMessagesAfterBanForPastDays: 7 // Delete the spammed messages after banning for the past x days.
+});
 
 //Obols Currency
 client.on("message", (message) => {
