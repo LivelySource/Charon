fdc0e8634a8d7bf85d29e919a50cff06004d0efd
LivelySource <weston.schmidt4@gmail.com>
2018-12-12 17:49:05 -0800

    go!

--- events/guildCreate.js
+++ events/guildCreate.js
@@ -3,6 +3,7 @@
 module.exports = (client, guild) => {
   client.user.setPresence({game: {name: `${client.guilds.size} Servers | ${client.settings.get("default").prefix}help `, type:0}});
   client.log("log", `New guild has been joined: ${guild.name} (${guild.id}) with ${guild.memberCount}`, "JOINED");
+  client.on("guildCreate", guild => {
     let serverOwner = guild.owner.userame
     let discriminator = guild.owner.discriminator
     let guildCreate = new Discord.RichEmbed()
@@ -13,5 +14,5 @@ module.exports = (client, guild) => {
     .setFooter(`Go see if ${serverOwner}${discriminator} needs your help!`)
     
   client.guilds.get(512733951794741248).channels.get(522577142630973444).send(guildCreate)
-  
+  });
 };
