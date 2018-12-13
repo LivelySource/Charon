97d8ef8100077d67990029dc6b725488087302ae
LivelySource <weston.schmidt4@gmail.com>
2018-12-12 18:05:31 -0800

    try now!

--- events/guildCreate.js
+++ events/guildCreate.js
@@ -1,7 +1,7 @@
 // This event executes when a new guild (server) is joined.
 const Discord =require('discord.js');
-
 module.exports = (client, guild) => {
+  const client = new Discord.Client();
   client.user.setPresence({game: {name: `${client.guilds.size} Servers | ${client.settings.get("default").prefix}help `, type:0}});
   client.log("guild", `New guild has been joined: ${guild.name} (${guild.id}) with ${guild.memberCount}`, "JOINED");
     let serverOwner = guild.owner.userame
