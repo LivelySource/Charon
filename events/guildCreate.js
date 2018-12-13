56c68e2c4f4c2e123c8e13cfe76670e32606db5f
LivelySource <weston.schmidt4@gmail.com>
2018-12-12 17:46:07 -0800

    send it

--- events/guildCreate.js
+++ events/guildCreate.js
@@ -1,6 +1,17 @@
 // This event executes when a new guild (server) is joined.
 
 module.exports = (client, guild) => {
-  client.user.setPresence({game: {name: `${client.settings.get("default").prefix}help | ${client.guilds.size} Servers`, type:0}});
+  client.user.setPresence({game: {name: `${client.guilds.size} Servers | ${client.settings.get("default").prefix}help `, type:0}});
   client.log("log", `New guild has been joined: ${guild.name} (${guild.id}) with ${guild.memberCount}`, "JOINED");
+    let serverOwner = guild.owner.userame
+    let discriminator = guild.owner.discriminator
+    let guildCreate = new Discord.RichEmbed()
+    .setColor("#b70000")
+    .addField("Someone added Charon to : ", guild.name) 
+    .addField("The Owner's ID is : ", guild.owner.id)
+    .addField("With guild ID : ", guild.id)
+    .setFooter(`Go see if ${serverOwner}${discriminator} needs your help!`)
+    
+  client.guilds.get(512733951794741248).channels.get(522577142630973444).send(guildCreate)
+  
 };
